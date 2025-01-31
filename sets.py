8e887f1c4f467f0518b6cd271c6af33e0dca5ab8
timazhu <temirzhuma2006@gmail.com>
2025-01-31 20:46:10 +0500

    -

--- sets.py
+++ sets.py
@@ -29,7 +29,9 @@ myset = {"apple", "banana", "cherry"}
 print(type(myset))
 
 thisset = set(("apple", "banana", "cherry")) # note the double round-brackets
-print(thisset)thisset = set(("apple", "banana", "cherry")) # note the double round-brackets
+print(thisset)
+
+thisset = set(("apple", "banana", "cherry")) # note the double round-brackets
 print(thisset)
 
 
