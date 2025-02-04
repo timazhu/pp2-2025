fea35b93437fecc21d4f4e1d3a23f1596b05b114
timazhu <temirzhuma2006@gmail.com>
2025-02-04 23:04:25 +0500

    m

---
+++ functions1.4.py
@@ -0,0 +1,15 @@
+def filter_prime(numbers):
+    primes = []
+    for num in numbers:
+        if num > 1:
+            for i in range(2, num):
+                if num % i == 0:
+                    break
+            else:
+                primes.append(num)
+    return primes
+
+#numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+#prime_numbers = filter_prime(numbers)
+#print(prime_numbers)
+#[2, 3, 5, 7, 11, 13]
