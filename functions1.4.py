50d265e820be845f7cb1c25dabb6f845a82fbc3d
timazhu <temirzhuma2006@gmail.com>
2025-02-06 23:28:37 +0500

    -

--- functions1.4.py
+++ functions1.4.py
@@ -1,3 +1,4 @@
+#You are given list of numbers separated by spaces. Write a function filter_prime which will take list of numbers as an agrument and returns only prime numbers from the list.
 def filter_prime(numbers):
     primes = []
     for num in numbers:
