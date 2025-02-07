fb89951add97bff5ec7dad3d52790502026dc0b3
timazhu <temirzhuma2006@gmail.com>
2025-02-07 21:09:44 +0500

    3

---
+++ classes3.py
@@ -0,0 +1,21 @@
+class Shape:
+    def __init__(self):
+        pass
+
+    def area(self):
+        return 0
+
+class Rectangle(Shape):
+    def __init__(self, length, width):
+        super().__init__()
+        self.length = length
+        self.width = width
+
+    def area(self):
+        return self.length * self.width
+
+    def __str__(self):
+        return f"Rectangle with area: {self.area()}"
+
+rectangle = Rectangle(7, 3)
+print(rectangle)
