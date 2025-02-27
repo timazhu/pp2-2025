db2139537caf0fce7547d4b273ebd9bf5e189cbd
timazhu <temirzhuma2006@gmail.com>
2025-02-27 23:28:42 +0500

    date2

--- lab4/date2.py
+++ lab4/date2.py
@@ -8,3 +8,7 @@ tomorrow = today + datetime.timedelta(days=1)
 print(yesterday.strftime('%Y-%m-%d'))
 print(today.strftime('%Y-%m-%d'))
 print(tomorrow.strftime('%Y-%m-%d'))
+
+#2025-02-26
+#2025-02-27
+#2025-02-28
