d95dd5aa733b639beb54da50a1c44ba531201732
timazhu <temirzhuma2006@gmail.com>
2025-02-27 23:25:10 +0500

    date1

--- lab4/date1.py
+++ lab4/date1.py
@@ -1,7 +1,8 @@
 #Write a Python program to subtract five days from current date.
 
-
 import datetime
-
 dt_now = datetime.datetime.now()
-print(dt_now)
+dt_minus_5 = dt_now - datetime.timedelta(days=5)
+print(dt_minus_5)
+
+#2025-02-22 23:23:53.073494
