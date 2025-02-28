9e75f0b2d70b13805542b6421e428c48e92505e2
timazhu <temirzhuma2006@gmail.com>
2025-02-28 18:15:42 +0500

    regex2

--- lab5/regex2.py
+++ lab5/regex2.py
@@ -1,29 +1,14 @@
 #Write a Python program that matches a string that has an 'a' followed by two to three 'b'.
 
 import re
+def text_match(text):
+        patterns = 'ab{2,3}'
+        if re.search(patterns,  text):
+                return 'YES'
+        else:
+                return('NO')
+print(text_match("abc"))
+print(text_match("aabbbcd"))
 
-def match_string(text):
-    pattern = r'ab{2,3}'
-    if re.search(pattern, text):
-        return True
-    else:
-        return False
-
-test_strings = ["a", "ab", "abb", "abbb", "abbbb", "ac", "b", "abc", "aab", "cabbbd"]
-
-for s in test_strings:
-    if match_string(s):
-        print(f"'{s}' matches the pattern.")
-    else:
-        print(f"'{s}' does not match the pattern.")
-
-#'a' does not match the pattern.
-#'ab' does not match the pattern.
-#'abb' matches the pattern.
-#'abbb' matches the pattern.
-#'abbbb' matches the pattern.
-#'ac' does not match the pattern.
-#'b' does not match the pattern.
-#'abc' does not match the pattern.
-#'aab' does not match the pattern.
-#'cabbbd' matches the pattern.
+#NO
+#YES
