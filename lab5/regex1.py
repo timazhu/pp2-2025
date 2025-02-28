89579c56514a21853060a0ad1de38286f373e035
timazhu <temirzhuma2006@gmail.com>
2025-02-28 18:13:35 +0500

    regex1

--- lab5/regex1.py
+++ lab5/regex1.py
@@ -1,27 +1,21 @@
 #Write a Python program that matches a string that has an 'a' followed by zero or more 'b''s.
 
 import re
+def text_match(text):
+        patterns = '^a(b*)$'
+        if re.search(patterns,  text):
+                return 'YES'
+        else:
+                return("NO")
+        
+print(text_match("ac"))
+print(text_match("abc"))
+print(text_match("a"))
+print(text_match("ab"))
+print(text_match("abb"))
 
-def match_string(text):
-    pattern = r'^ab*$'
-
-    if re.match(pattern, text):
-        return True
-    else:
-        return False
-
-test = ["a", "ab", "abb", "ac", "b", "abc", "aab"]
-
-for s in test:
-    if match_string(s):
-        print(f"'{s}' matches the pattern.")
-    else:
-        print(f"'{s}' does not match the pattern.")
-
-#'a' matches the pattern.
-#'ab' matches the pattern.
-#'abb' matches the pattern.
-#'ac' does not match the pattern.
-#'b' does not match the pattern.
-#'abc' does not match the pattern.
-#'aab' does not match the pattern.
+#NO
+#NO
+#YES
+#YES
+#YES
