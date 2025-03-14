c3b2aa0557b461aebd9649bcfb2df93e453bf7a0
timazhu <temirzhuma2006@gmail.com>
2025-03-14 22:14:36 +0500

    redball

--- lab7/pygame3.py
+++ lab7/pygame3.py
@@ -2,8 +2,8 @@ import pygame
 
 pygame.init()
 
-screen_width = 600
-screen_height = 400
+screen_width = 1000
+screen_height = 600
 screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption("Moving Ball")
 
