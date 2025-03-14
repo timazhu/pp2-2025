4ed5ef029eae4eb44628318314e09565292578b7
timazhu <temirzhuma2006@gmail.com>
2025-03-14 21:13:30 +0500

    music

--- lab7/pygame2.py
+++ lab7/pygame2.py
@@ -6,21 +6,26 @@ pygame.init()
 screen = pygame.display.set_mode((500, 500))
 pygame.display.set_caption("Music Player")
 
-music_directory = "C:\Users\User\Desktop\PP2\lab7"
-os.chdir(music_directory)
+music_directory = r"C:\Users\User\Desktop\PP2\lab7"
+try:
+    os.chdir(music_directory)
+except FileNotFoundError:
+    print(f"Error: Directory '{music_directory}' not found.")
+    pygame.quit()
+    exit()
 
 music_files = [file for file in os.listdir() if file.endswith(".mp3")]
+if not music_files:
+    print("No MP3 files found in the directory.")
+    pygame.quit()
+    exit()
 
 current_track = 0
 paused = False
-
 pygame.mixer.music.load(music_files[current_track])
+pygame.mixer.music.play()
 
 font = pygame.font.Font(None, 24)
-
-kaif = pygame.image.load("../kaif.jpg")
-
-kaif = pygame.transform.scale(kaif, (200, 200))
 
 running = True
 while running:
@@ -43,12 +48,11 @@ while running:
                 current_track = (current_track - 1) % len(music_files)
                 pygame.mixer.music.load(music_files[current_track])
                 pygame.mixer.music.play()
-    
+
     screen.fill((255, 255, 255))
+
     instructions = font.render("Spacebar: Play/Pause, Left Arrow: Previous, Right Arrow: Next", True, (0, 0, 0))
     screen.blit(instructions, (10, 10))
-
-    screen.blit(kaif, (150, 100))
 
     pygame.display.flip()
 
